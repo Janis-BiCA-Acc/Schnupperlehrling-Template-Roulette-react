08a89ab287e892b454b7bf72a002320ed8800fbb
Janis Hofstetter <hoj@bica.ch>
2023-12-05 09:22:36 +0100

    init

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello, React 18 Alpha!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
